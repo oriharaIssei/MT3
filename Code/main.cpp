32dccf71d83a60c71766092e3a1cbac83eaa3f07
Amaimono <k023g0048@g.neec.ac.jp>
2024-04-12 12:36:17 +0900

    MT3_01_02

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,16 +1,33 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <imgui.h>
+#include <stdint.h>
 
-#include "Vec3.h"
 #include <MyMatrix4x4.h>
+#include <Vec3.h>
 
-const char kWindowTitle[]="LC1B_08_オリハライッセイ";
+const char kWindowTitle[]="LE2A_08_オリハライッセイ_MT3";
 
-struct Triangle {
-	Vec3 vert[3];
-	Vec3 scVert[3];
-	MyMatrix4x4 worldMa;
-	bool isFront();
+void DrawGrid(const MyMatrix4x4 &viewProjectionMa, const MyMatrix4x4 &viewPortMa);
+
+struct TransformData {
+	Vec3 scale;
+	Vec3 rotate;
+	Vec3 translate;
 };
+
+struct Camera {
+	TransformData transformData;
+	MyMatrix4x4 vpMa;
+};
+struct Sphere {
+	TransformData transformData;
+	float radius;
+	MyMatrix4x4 worldMa;
+};
+void DrawGrid(const MyMatrix4x4 &viewProjectionMa, const MyMatrix4x4 &viewPortMa);
+void DrawSphere(const Sphere &sphere, const MyMatrix4x4 &viewProjectionMa, const MyMatrix4x4 &viewPortMa, uint32_t color);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -22,27 +39,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, static_cast<int>(kWindowWidth), static_cast<int>(kWindowHeight));
 	
 	//==============初期化====================
-	Triangle triangle;
-	triangle.vert[0]={ 0.0f, 1.5f, 0.0f };
-	triangle.vert[1]={ 1.5f, 0.0f, 0.0f };
-	triangle.vert[2]={ -1.5f, 0.0f, 0.0f };
+	Camera camera;
+	camera.transformData.scale={ 1.0f,1.0f,1.0f };
+	camera.transformData.rotate={ 0.26f,0.0f,0.0f };
+	camera.transformData.translate={ 0.0f,1.9f,-6.49f };
 
-	Vec3 rotate={ 0.0f,0.0f,0.0f };
-
-	Vec3 translate={ 0.0f,0.0f,0.0f };
-
-	triangle.worldMa=MakeMatrix::Affine({ 1.0f,1.0f,1.0f }, rotate, translate);
-
-	MyMatrix4x4 cameraMa=MakeMatrix::Affine({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,-6.49f });
-	MyMatrix4x4 viewMa=cameraMa.Inverse();
+	Sphere sphere;
+	sphere.transformData.scale={ 1.0f,1.0f,1.0f };
+	sphere.transformData.rotate={ 0.0f,0.0f,0.0f };
+	sphere.transformData.translate={ 0.0f,0.0f,1.0f };
+	sphere.radius=1.0f;
 
 	MyMatrix4x4 projectionMa=MakeMatrix::PerspectiveFov(0.45f, kWindowWidth / kWindowHeight, 0.1f, 100.0f);
-
-	MyMatrix4x4 wvpMa=triangle.worldMa * (viewMa * projectionMa);
+	camera.vpMa=MakeMatrix::Affine(
+		camera.transformData.scale,
+		camera.transformData.rotate,
+		camera.transformData.translate
+	) * projectionMa;
 
 	MyMatrix4x4 viewPortMa=MakeMatrix::ViewPort(0.0f, 0.0f, kWindowWidth, kWindowHeight, 0.0f, 1.0f);
-
-	Vec3 cross=Vec3(1.2f, -3.9f, 2.5f).Cross({ 2.8f,0.4f,-1.3f });
 
 	// キー入力結果を受け取る箱
 	char keys[256]={ 0 };
@@ -61,31 +76,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		// rotate
-		rotate.y+=0.03f;
+		ImGui::Begin("window");
+		ImGui::DragFloat3("camera Translate", &camera.transformData.translate.x, 0.01f);
+		ImGui::DragFloat3("camera Rotate", &camera.transformData.rotate.x, 0.01f);
+		ImGui::DragFloat3("sphere CenterPos", &sphere.transformData.translate.x, 0.01f);
+		ImGui::DragFloat3("sphere Rotate", &sphere.transformData.rotate.x, 0.01f);
+		ImGui::End();
 
-		// move
-		if(Novice::CheckHitKey(DIK_W)) {
-			translate.z-=0.1f;
-		}
-		if(Novice::CheckHitKey(DIK_S)) {
-			translate.z+=0.1f;
-		}
-		if(Novice::CheckHitKey(DIK_A)) {
-			translate.x-=0.1f;
-		}
-		if(Novice::CheckHitKey(DIK_D)) {
-			translate.x+=0.1f;
-		}
+		camera.vpMa=MakeMatrix::Affine(
+			camera.transformData.scale,
+			camera.transformData.rotate,
+			camera.transformData.translate
+		).Inverse() * projectionMa;
 
-
-		triangle.worldMa=MakeMatrix::Affine({ 1.0f,1.0f,1.0f }, rotate, translate);
-		wvpMa=triangle.worldMa * viewMa * projectionMa;
-
-		for(int i=0; i < 3; i++) {
-			Vec3 ndcPos=Transform(triangle.vert[i], wvpMa);
-			triangle.scVert[i]=Transform(ndcPos, viewPortMa);
-		}
+		sphere.worldMa=MakeMatrix::Affine({ 1.0f,1.0f ,1.0f }, sphere.transformData.rotate, sphere.transformData.translate);
 
 		///
 		/// ↑更新処理ここまで
@@ -95,18 +99,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		if(triangle.isFront()) {
-			Novice::DrawTriangle(
-				static_cast<int>(triangle.scVert[0].x),
-				static_cast<int>(triangle.scVert[0].y),
-				static_cast<int>(triangle.scVert[1].x),
-				static_cast<int>(triangle.scVert[1].y),
-				static_cast<int>(triangle.scVert[2].x),
-				static_cast<int>(triangle.scVert[2].y),
-				RED,
-				kFillModeSolid
-			);
-		}
+		DrawGrid(camera.vpMa, viewPortMa);
+		DrawSphere(sphere, camera.vpMa, viewPortMa, BLACK);
 
 		///
 		/// ↑描画処理ここまで
@@ -125,9 +119,102 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Finalize();
 	return 0;
 }
-bool Triangle::isFront() {
-	if(Vec3(scVert[0] - scVert[1]).Cross(scVert[1] - scVert[2]).z > 0.0f) {
-		return true;
+void DrawGrid(const MyMatrix4x4 &viewProjectionMa, const MyMatrix4x4 &viewPortMa) {
+	// Grid の半分の幅
+	constexpr float kGridHalfWidth=2.0f;
+	// 分割数
+	constexpr uint32_t kSubDivision=10;
+	// 1 つ分の長さ
+	constexpr float kGridEvery=(kGridHalfWidth * 2.0f) / static_cast<float>(kSubDivision);
+
+	for(uint32_t xIndex=0; xIndex <= kSubDivision; ++xIndex) {
+		// 線の始点
+		Vec3 startPos={ -kGridHalfWidth + kGridEvery * xIndex, 0.0f,  -kGridHalfWidth };
+		// 線の終点
+		Vec3 endPos={ -kGridHalfWidth + kGridEvery * xIndex, 0.0f,  kGridHalfWidth };
+
+		// 始点と終点をビュープロジェクション変換
+		Vec3 ndcStartPos=Transform(startPos, viewProjectionMa);
+		Vec3 ndcEndPos=Transform(endPos, viewProjectionMa);
+
+		// NDCからスクリーン座標へ変換
+		Vec3 scStartPos=Transform(ndcStartPos, viewPortMa);
+		Vec3 scEndPos=Transform(ndcEndPos, viewPortMa);
+
+		// 線を描画
+		Novice::DrawLine(
+			static_cast<int>(scStartPos.x), static_cast<int>(scStartPos.y),
+			static_cast<int>(scEndPos.x), static_cast<int>(scEndPos.y),
+			0xaaaaaaff
+		);
 	}
-	return false;
+	for(uint32_t zIndex=0; zIndex <= kSubDivision; ++zIndex) {
+		// 線の始点
+		Vec3 startPos={ kGridHalfWidth ,0.0f, -kGridHalfWidth + kGridEvery * zIndex };
+		// 線の終点
+		Vec3 endPos={ -kGridHalfWidth ,0.0f , -kGridHalfWidth + kGridEvery * zIndex };
+
+		// 始点と終点をビュープロジェクション変換
+		Vec3 ndcStartPos=Transform(startPos, viewProjectionMa);
+		Vec3 ndcEndPos=Transform(endPos, viewProjectionMa);
+
+		// NDCからスクリーン座標へ変換
+		Vec3 scStartPos=Transform(ndcStartPos, viewPortMa);
+		Vec3 scEndPos=Transform(ndcEndPos, viewPortMa);
+
+		// 線を描画
+		Novice::DrawLine(
+			static_cast<int>(scStartPos.x), static_cast<int>(scStartPos.y),
+			static_cast<int>(scEndPos.x), static_cast<int>(scEndPos.y),
+			0xaaaaaaff
+		);
+	}
+}
+
+void DrawSphere(const Sphere &sphere, const MyMatrix4x4 &viewProjectionMa, const MyMatrix4x4 &viewPortMa, uint32_t color) {
+	constexpr uint32_t kSubDivision=16;
+	// 経度分割 1 つ分の角度
+	constexpr float kLatEvery=static_cast<float>(M_PI) / static_cast<float>(kSubDivision);
+	// 緯度分割 1 つ分の角度
+	constexpr float kLonEvery=static_cast<float>(M_PI) * 2.0f / static_cast<float>(kSubDivision);
+
+	Vec3 pos[3];
+
+	for(uint32_t latIndex=0; latIndex < kSubDivision; ++latIndex) {
+		// -π/2 ~ π/2
+		float lat=-static_cast<float>(M_PI) / 2.0f + (kLatEvery * latIndex);
+		for(uint32_t lonIndex=0; lonIndex < kSubDivision; ++lonIndex) {
+			float lon=lonIndex * kLonEvery;
+			Vec3 a, b, c;
+			a=Vec3({ std::cosf(lat) * std::cosf(lon),std::sinf(lat),std::cosf(lat) * std::sinf(lon) }) * sphere.radius;
+			b=Vec3({ std::cosf(lat + kLatEvery) * std::cosf(lon),std::sinf(lat + kLatEvery),std::cosf(lat + kLatEvery) * std::sinf(lon) }) * sphere.radius;
+			c=Vec3({ std::cosf(lat) * std::cosf(lon + kLonEvery),std::sinf(lat),std::cosf(lat) * std::sinf(lon + kLonEvery) }) * sphere.radius;
+
+			// ndc
+			pos[0]=Transform(a, sphere.worldMa * viewProjectionMa);
+			pos[1]=Transform(b, sphere.worldMa * viewProjectionMa);
+			pos[2]=Transform(c, sphere.worldMa * viewProjectionMa);
+			// screen
+			pos[0]=Transform(pos[0], viewPortMa);
+			pos[1]=Transform(pos[1], viewPortMa);
+			pos[2]=Transform(pos[2], viewPortMa);
+
+			// ab
+			Novice::DrawLine(
+				static_cast<int>(pos[0].x),
+				static_cast<int>(pos[0].y),
+				static_cast<int>(pos[1].x),
+				static_cast<int>(pos[1].y),
+				color
+			);
+			// bc
+			Novice::DrawLine(
+				static_cast<int>(pos[0].x),
+				static_cast<int>(pos[0].y),
+				static_cast<int>(pos[2].x),
+				static_cast<int>(pos[2].y),
+				color
+			);
+		}
+	}
 }
